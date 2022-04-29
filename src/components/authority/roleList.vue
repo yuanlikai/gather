b535b2a4503595ed1be05c2dc42e39d11bbbf7c9
袁理开 <1091372693@qq.com>
2022-04-29 20:54:56 +0800

    Update roleList.vue

--- src/components/authority/roleList.vue
+++ src/components/authority/roleList.vue
@@ -201,6 +201,7 @@ export default {
                     _this.spinShow = true;
                     _this.addAccount = true;
                     _this.id = params.row.id;
+                    _this.formValidate.id = params.row.id;
                     _this.handleReset('formValidate');
                     _this.formValidate.remark = params.row.remark;
                     _this.formValidate.roleName = params.row.roleName;
@@ -326,26 +327,27 @@ export default {
       _this.$refs[name].validate((valid) => {
         if (valid) {
           _this.modal_loading = true;
+          _this.Axios.post('/EditRole.ashx', {
+            id: Number(_this.formValidate.id),
+            roleName: _this.formValidate.roleName,        //角色名称
+            roleCode: _this.formValidate.roleCode,        //角色编号
+            remark: _this.formValidate.remark,            //备注
+            menuId1: menuId1.join(','),
+            menuId2: menuId2.join(','),
+            menuId3: menuId3.join(','),
+            IsSupplier: _this.formValidate.supplierExclusive ? 0 : 1,   //是否供应商专属 true为是 需要传
+          }).then(res => {
+            if (res.data.error === 0) {
+              _this.addAccount = false;
+              _this.getTreeList();
+              _this.$Message.success('添加成功')
+            } else {
+              _this.$Message.error(res.data.errorMsg)
+            }
+            _this.modal_loading = false;
+          })
+          return
           if (_this.id === '') {
-            _this.Axios.post('/EditRole.ashx', {
-              id: _this.formValidate.id,
-              roleName: _this.formValidate.roleName,        //角色名称
-              roleCode: _this.formValidate.roleCode,        //角色编号
-              remark: _this.formValidate.remark,            //备注
-              menuId1: menuId1.join(','),
-              menuId2: menuId2.join(','),
-              menuId3: menuId3.join(','),
-              IsSupplier: _this.formValidate.supplierExclusive ? 0 : 1,   //是否供应商专属 true为是 需要传
-            }).then(res => {
-              if (res.data.error === 0) {
-                _this.addAccount = false;
-                _this.getTreeList();
-                _this.$Message.success('添加成功')
-              } else {
-                _this.$Message.error(res.data.errorMsg)
-              }
-              _this.modal_loading = false;
-            })
           } else {
             _this.Axios.post('/Manage/Role/updateRole', _this.Qs.stringify({
               id: _this.id,
