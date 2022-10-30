5e722e7fbdcca27ca1df1b5f0331c0010a1a15a0
袁理开 <1091372693@qq.com>
2022-10-30 13:16:58 +0800

    修改主题色

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,16 +1,68 @@
+<style>
+  body .ivu-menu-light {
+    background: none;
+  }
+  
+  body .ivu-menu-light.ivu-menu-horizontal .ivu-menu-item, .ivu-menu-light.ivu-menu-horizontal .ivu-menu-submenu {
+    color: hsla(0, 0%, 100%, .7);
+  }
+  
+  body .ivu-menu-light.ivu-menu-horizontal .ivu-menu-item-active, .ivu-menu-light.ivu-menu-horizontal .ivu-menu-item:hover, .ivu-menu-light.ivu-menu-horizontal .ivu-menu-submenu-active, .ivu-menu-light.ivu-menu-horizontal .ivu-menu-submenu:hover {
+    color: #ffffff;
+    border-bottom: 3px solid #2d8cf0;
+    border-bottom-color: #4fe3c1;
+  }
+  
+  body .layout-header-bar {
+    color: #ffffff;
+    background: #2173dc;
+    background: -webkit-gradient(linear, left top, right top, from(#1d42ab), color-stop(#2173dc), to(#1e93ff));
+    background: linear-gradient(
+      90deg, #1d42ab, #2173dc, #1e93ff);
+  }
+</style>
 <template>
   <div class="layout">
-    <Layout class="">
-      <Sider breakpoint="xs" class="Sider2" ref="side1" hide-trigger collapsible :collapsed-width="0" :width="256"
+    <Header :style="{padding: 10,position:'fixed',width:'100%',zIndex:'999',display:'flex'}" class="layout-header-bar">
+      <Icon @click.native="collapsedSider" :class="rotateIcon"
+            :style="{margin: '0 24px','line-height':'64px',cursor:'pointer',float:'left'}"
+            type="md-menu"
+            size="24"></Icon>
+      <Menu v-if="isMenu" @on-select="selectMenu1" mode="horizontal" :active-name="menu1" id="ivu-menu"
+            style="float: left">
+        <MenuItem v-for="(item,index) in menuList" :key="index" :name="item.numb">
+          {{item.name}}
+        </MenuItem>
+      </Menu>
+      <Menu mode="horizontal" type="light" active-name="1" style="margin-left: auto;margin-right: 20px;color: #ffffff">
+        <div class="layout-logo"></div>
+        <div class="layout-nav">
+          <span style="font-size: 12px;font-weight: 600;">{{supplier.supplierName}}</span>
+          <Dropdown trigger="click" style="margin-left: 20px;cursor: pointer;" @on-click="out">
+            <Avatar style="margin-right: 8px"
+                    src="https://ylcgenterprise.oss-cn-shanghai.aliyuncs.com/touxiang.jpg"/>
+            {{user}}
+            <Icon type="ios-arrow-down"/>
+            <DropdownMenu slot="list">
+              <DropdownItem name="1">修改密码</DropdownItem>
+              <DropdownItem name="2">退出登录</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </div>
+      </Menu>
+    </Header>
+    <Layout>
+      <Sider :style="{background: '#fff',position:'fixed',top:'60px'}" breakpoint="xs" class="Sider2" ref="side1"
+             hide-trigger collapsible :collapsed-width="0" :width="256"
              v-model="isCollapsed"
              style="overflow-x: auto;height: 100vh;">
-        <Menu v-if="isMenu" @on-select="selectMenu" :theme="'dark'" :active-name="menu"
+        <Menu v-if="isMenu" @on-select="selectMenu" :theme="'light'" :active-name="menu"
               width="256px">
-          <div class="i-layout-sider-logo i-layout-sider-logo-dark">
-            <a @click="operateMuen('1','1-1-2')" class="i-link i-link-color">
-              <img src="../assets/img/logo-wat.png">
-            </a>
-          </div>
+          <!--<div class="i-layout-sider-logo i-layout-sider-logo-dark">-->
+          <!--<a @click="operateMuen('1','1-1-2')" class="i-link i-link-color">-->
+          <!--<img src="../assets/img/logo-wat.png">-->
+          <!--</a>-->
+          <!--</div>-->
           <div v-for="(item,index) in menuList" :key="index" v-show="item.numb===menu1">
             <MenuGroup v-for="(itema,indexa) in item.children" :key="indexa" :title="itema.name">
               <MenuItem v-for="(itemb,indexb) in itema.children" :key="indexb" :to="itemb.to" :name="itemb.numb">
@@ -21,41 +73,14 @@
           </div>
         </Menu>
       </Sider>
-      <Layout :class="isCollapsed?'menucls':'menucla'">
-        <Header :style="{padding: 10}" class="layout-header-bar">
-          <Icon @click.native="collapsedSider" :class="rotateIcon"
-                :style="{margin: '0 24px','line-height':'64px',cursor:'pointer',float:'left'}"
-                type="md-menu"
-                size="24"></Icon>
-          <Menu v-if="isMenu" @on-select="selectMenu1" mode="horizontal" :active-name="menu1" id="ivu-menu"
-                style="float: left">
-            <MenuItem v-for="(item,index) in menuList" :key="index" :name="item.numb">
-              {{item.name}}
-            </MenuItem>
-          </Menu>
-          <Menu mode="horizontal" type="light" id="ivu-menua" style="float: right;margin-right: 20px">
-            <div class="layout-logo"></div>
-            <div class="layout-nav">
-              <span style="font-size: 12px;font-weight: 600;color: #333;">{{supplier.supplierName}}</span>
-              <router-link to="/log" v-show="supplier.userType==='SUPER'">
-                <Tooltip content="日志" placement="bottom">
-                  <Icon style="margin-left: 20px" size="18" color="#555555" type="md-ionic"/>
-                </Tooltip>
-              </router-link>
-              <Dropdown trigger="click" style="margin-left: 20px;cursor: pointer;" @on-click="out">
-                <Avatar style="margin-right: 8px"
-                        src="https://ylcgenterprise.oss-cn-shanghai.aliyuncs.com/touxiang.jpg"/>
-                {{user}}
-                <Icon type="ios-arrow-down"/>
-                <DropdownMenu slot="list">
-                  <DropdownItem name="1">修改密码</DropdownItem>
-                  <DropdownItem name="2">退出登录</DropdownItem>
-                </DropdownMenu>
-              </Dropdown>
-            </div>
-          </Menu>
-        </Header>
-        <router-view :supplier="supplier" @operateMuen="operateMuen"></router-view>
+      <Layout>
+        <Content :style="{
+        transition: 'all .2s ease-in-out',
+        marginTop: '60px',
+        paddingLeft: isCollapsed?'0':'256px'
+        }">
+          <router-view :supplier="supplier" @operateMuen="operateMuen"></router-view>
+        </Content>
       </Layout>
     </Layout>
     <div @click="collapsedSider" v-if="!isCollapsed" class="zz-fixed"></div>
@@ -169,7 +194,7 @@
       tagAffair(i) {
         this.$store.commit('increment', i)
       },
-
+      
       //退出登录
       out(i) {
         const _this = this;
@@ -185,16 +210,16 @@
           _this.$router.push('/');
         }
       },
-
+      
       //获取用户信息
       getCurrentUser() {
         const _this = this;
         _this.Axios.get('/getCurrentUser.ashx').then(res => {
           _this.supplier = res.data.data;
-          localStorage.setItem('supplierId',res.data.data.supplierId)
+          localStorage.setItem('supplierId', res.data.data.supplierId)
         })
       },
-
+      
       //子组件改变菜单选择
       operateMuen(a, b) {
         this.menu1 = a;
@@ -220,11 +245,11 @@
   #ivu-menu:after, #ivu-menua:after {
     display: none;
   }
-
+  
   .swiper-container {
     width: 100%;
   }
-
+  
   .swiper-slide {
     width: auto; /*根据内容调整宽度*/
   }
